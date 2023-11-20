95528301934a777554ed47d2e9db15409e41fcc0
Joseph Hunter <justjahunter19@gmail.com>
2023-11-19 22:23:51 -0500

    date selection and update feature in calendar

--- client/src/index.ts
+++ client/src/index.ts
@@ -6,26 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
   main();
 });
 
-async function main() {
+function main(): void {
+  // Initialize calendar
   initializeCalendar();
 
-  // add event listeners to calendar dates
-  initializeCalendarEvents();
-  document
-    .getElementById("schedule-generate-button")
-    ?.addEventListener("click", async () => {
-      try {
-        const calendar = document.getElementById(
-          "calendar"
-        ) as HTMLInputElement;
-        const date: string = calendar.value;
-        const schedule: ApiResponse = await generateSchedule(date);
-        console.log(schedule);
-        printScheduleToDocument(schedule);
-      } catch (error) {
-        console.error("Error: ", error);
-      }
-    });
+  // set up schedule generator
+  initializeGenerator();
 
   document
     .getElementById("schedule-getter-button")
@@ -45,9 +31,10 @@ async function main() {
 }
 
 function initializeCalendar(): void {
-  let date = new Date();
-  let year = date.getFullYear();
-  let month = date.getMonth();
+  // current date
+  let date: Date = new Date();
+  let year: number = date.getFullYear();
+  let month: number = date.getMonth();
 
   const day = document.querySelector(".calendar-dates") as HTMLElement;
   const currentDate = document.querySelector(
@@ -74,10 +61,10 @@ function initializeCalendar(): void {
 
   // Function that generates the actual calendar
   const manipulate = () => {
-    let firstDay = new Date(year, month, 1).getDay();
-    let lastDate = new Date(year, month + 1, 0).getDate();
-    let dayEnd = new Date(year, month, lastDate).getDay();
-    let previousMonthDate = new Date(year, month, 0).getDate();
+    let firstDay: number = new Date(year, month, 1).getDay();
+    let lastDate: number = new Date(year, month + 1, 0).getDate();
+    let dayEnd: number = new Date(year, month, lastDate).getDay();
+    let previousMonthDate: number = new Date(year, month, 0).getDate();
 
     // Temporary variable to store the generated calendar html
     let tableDataArray: Array<string> = [];
@@ -118,6 +105,37 @@ function initializeCalendar(): void {
 
     const calendarDatesString: string = tableDataArray.join("");
     day.innerHTML = calendarDatesString;
+
+    // Attach listeners to each calendar for selecting a date
+    const calendarDays: NodeListOf<HTMLElement> = document.querySelectorAll(
+      ".calendar-dates tr td"
+    );
+
+    let formattedDate = formatDate(date);
+    console.log(formattedDate);
+
+    calendarDays.forEach((day) => {
+      day.addEventListener("click", () => {
+        // Remove the current active day
+        calendarDays.forEach((otherDay) => {
+          if (otherDay.classList.contains("today-active"))
+            otherDay.classList.replace("today-active", "active");
+        });
+        // Set new active day
+        if (day.classList.contains("active")) {
+          day.classList.replace("active", "today-active");
+
+          // FIXME: prints wrong date when year changes. Should always default to
+          // the original date.
+
+          //code to change formattedDate to the current one
+          const newDay = day.innerHTML;
+          date = new Date(`${month + 1} ${newDay}, ${year}`);
+          formattedDate = formatDate(date);
+          console.log(formattedDate);
+        }
+      });
+    });
   };
 
   manipulate();
@@ -143,14 +161,42 @@ function initializeCalendar(): void {
   });
 }
 
-// adds event listeners to each day in calendar
-function initializeCalendarEvents(): void {
-  const calendarDays = document.querySelectorAll(".calendar-dates tr td");
-  calendarDays.forEach((day) => {
-    day.addEventListener("click", () => {
-      if (day.classList.contains("active")) {
-        day.classList.replace("active", "today-active");
+function initializeGenerator(): void {
+  document
+    .getElementById("schedule-generate-button")
+    ?.addEventListener("click", async () => {
+      try {
+        const calendar = document.getElementById(
+          "calendar"
+        ) as HTMLInputElement;
+        const date: string = calendar.value;
+        const schedule: ApiResponse = await generateSchedule(date);
+        console.log(schedule);
+        printScheduleToDocument(schedule);
+      } catch (error) {
+        console.error("Error: ", error);
       }
     });
+}
+
+/**
+ *
+ *
+ *
+ * Maybe it could check to see if the next service corresponds with the selected
+ * date, and if not, the message text says "There is no plan for the date of [], but would
+ * you like to generate a plan for the next service on []?
+ *
+ * the logic to print the date to the screen will be similar logic to get the date over to
+ * the schedule generator
+ *
+ *
+ */
+
+function formatDate(date: Date): string {
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   });
 }
